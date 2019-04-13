bbb6edd8b1108bd14e517de0ade57ccea43d89a6
Adam Streck <adam.streck@gmail.com>
2019-04-13 14:56:39 +0200

    Controls first version.

---
+++ Assets/Scripts/MoveBlock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlock : MonoBehaviour
+{
+    private float ROTATION_SPEED = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 
+        var mousePosition = Input.mousePosition;
+        mousePosition.z = 10f;
+        var screenPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        screenPosition.y = 4f;
+        transform.position = screenPosition;
+        
+        // 
+        if (Input.GetMouseButton(0))
+        {
+            transform.RotateAround(transform.position, Camera.main.transform.forward, Time.deltaTime * 90f * ROTATION_SPEED);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            transform.RotateAround(transform.position, Camera.main.transform.forward, Time.deltaTime * -90f * ROTATION_SPEED);
+        }
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            enabled = false;
+        }
+    }
+}
--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -1,17 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
+    private const float ROTATION_SPEED = 1f;
+    private const float MOVEMENT_SPEED = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey((KeyCode.A)))
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, Time.deltaTime * 90f * ROTATION_SPEED);
+        }
+
+        if (Input.GetKey((KeyCode.D)))
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, Time.deltaTime * -90f * ROTATION_SPEED);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * Time.deltaTime * MOVEMENT_SPEED;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * Time.deltaTime * MOVEMENT_SPEED;
+        }
+
+
+        var position = transform.position;
+        position = new Vector3(position.x, Mathf.Clamp(position.y, 2f, 100f), position.z);
+        transform.position = position;
     }
 }
