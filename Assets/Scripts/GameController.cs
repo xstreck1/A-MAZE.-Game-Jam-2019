c9544ad8532b2396708c9b80cd822cb0abf8a196
Eline Ledent <16385013+ElineLedent@users.noreply.github.com>
2019-04-13 19:45:10 +0200

    end of game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     [SerializeField] private Text heightText;
+    [SerializeField] private Text GameOverText;
     [SerializeField] private BoxCollider WaterCollider;
 
     private BlockSpawner _blockManager;
@@ -16,6 +17,8 @@ public class GameController : MonoBehaviour
     private float HEIGHT_OFFSET = 5f;
 
     public float SpawnInterval = 3f;
+
+    private bool _gameOver = false;
 
     private void Start()
     {
@@ -37,6 +40,17 @@ public class GameController : MonoBehaviour
         SpawnNewBlock(SpawnInterval);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        _gameOver = true;
+        DisplayGameOverText();
+    }
+
+    private void DisplayGameOverText()
+    {
+        GameOverText.text = "Your tower reached a height of " + GetTotalHeight();
+    }
+
     private void SpawnNewBlock(float time)
     {
         StartCoroutine(SpawnCoRoutine(time));
@@ -51,6 +65,8 @@ public class GameController : MonoBehaviour
 
     private void GiveBlockToInputManager()
     {
+        if (_gameOver) { return; }
+
         var block = _blockManager.GetNewBlock();
         _inputManager.GiveBlock(block, GetTotalHeight() + HEIGHT_OFFSET);
     }
