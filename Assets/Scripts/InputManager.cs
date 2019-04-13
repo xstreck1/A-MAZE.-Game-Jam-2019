036a9ace1396f8ab6707b2247dc3de7539fe4d74
Adam Streck <adam.streck@gmail.com>
2019-04-13 16:44:35 +0200

    Update to input manager.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,31 +6,44 @@ public class InputManager : MonoBehaviour
 {
     private float ROTATION_SPEED = 1f;
 
+    Block CurrentBlock { get; set; }
+
+    public void GiveBlock(Block block)
+    {
+        CurrentBlock = block;
+    }
+
     // Update is called once per frame
     private void Update()
     {
-        // 
+        if (!CurrentBlock)
+            return;
+
+        // Movement
         var mousePosition = Input.mousePosition;
         mousePosition.z = RotateCamera.Depth;
         var screenPosition = Camera.main.ScreenToWorldPoint(mousePosition);
         screenPosition.y = 4f;
         transform.position = screenPosition;
-        
-        // 
+
+        // Rotation
         if (Input.GetMouseButton(0))
         {
-            transform.RotateAround(transform.position, RotateCamera.Singleton.transform.forward, Time.deltaTime * 90f * ROTATION_SPEED);
+            transform.RotateAround(transform.position, RotateCamera.Singleton.transform.forward,
+                Time.deltaTime * 90f * ROTATION_SPEED);
         }
 
         if (Input.GetMouseButton(1))
         {
-            transform.RotateAround(transform.position, RotateCamera.Singleton.transform.forward, Time.deltaTime * -90f * ROTATION_SPEED);
+            transform.RotateAround(transform.position, RotateCamera.Singleton.transform.forward,
+                Time.deltaTime * -90f * ROTATION_SPEED);
         }
-        
+
+        // Let go
         if (Input.GetKey(KeyCode.Space))
         {
             GetComponent<Rigidbody>().isKinematic = false;
-            enabled = false;
+            CurrentBlock = null;
         }
     }
 }
